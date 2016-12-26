03da2a22c25c89921c73db195092561618290517
xJoker <xjoker@xjoker.us>
2016-12-26 17:42:31 +0800

    增加建站功能

--- API_Test/Program.cs
+++ API_Test/Program.cs
@@ -11,6 +11,9 @@ namespace API_Test
     {
         static void Main(string[] args)
         {
+
+            var a = "{\"Data\":\"执行完成!\"}";
+            Console.WriteLine(a.Length.ToString());
             //初始化
             Salt_API_Function.APIurl = "https://192.168.72.60:1559/";
             Salt_API_Function.loginRequestInfo = new LoginRequestType()
@@ -24,19 +27,20 @@ namespace API_Test
             if (Salt_API_Function.Login() != null)
             {
                 Console.WriteLine("Login Success");
-                Console.WriteLine("Token: "+ RequestType.xAuthToken);
+                Console.WriteLine("Token: " + RequestType.xAuthToken);
             }
             else
             {
                 Console.WriteLine("Login Failed");
             }
 
+            //var bb = Salt_API_Function.SVNOperation("D:\\Web", SVNOperation.checkout,new List<string> { "192.168.72.68","192.168.72.69"},"salt","saltsalt", "https://salt-minion-2008-2:8443/svn/salt/");
+            var ccc = Salt_API_Function.CreateIISSite(new List<string> { "192.168.72.68", "192.168.72.69" }, siteName: "salt", svnUrl: "https://salt-minion-2008-2:8443/svn/salt/", svnUsername: "salt", svnPassword: "saltsalt",sitePath: "D:\\web\\salt",apppoolVersion:"v4.0",apppoolModel:"1",apppoolEnable32bit:false, siteProtocol:"http");
+            /////var bnbbb = Salt_API_Function.IISOperation(new List<string> { "192.168.72.68", "192.168.72.69" }, IISOperation.StopSite, new List<string> { "333.com" });
 
-            ///var bnbbb = Salt_API_Function.IISOperation(new List<string> { "192.168.72.68", "192.168.72.69" }, IISOperation.StopSite, new List<string> { "333.com" });
+            //var ccc = Salt_API_Function.GetSiteList("192.168.72.69");
 
-            var ccc = Salt_API_Function.GetSiteList("192.168.72.69");
-
-            ////Salt_API_Function.GetAppPoolList("*");
+            //////Salt_API_Function.GetAppPoolList("*");
 
             ////var cc = Salt_API_Function.SVNOperation(@"D:\Web\Repo120161012155749", SVNOperation.info, new List<string> { "192.168.72.68" }, st: SVNType.Order, fun: "xjoker_svn.info");
             //var cc =Salt_API_Function.WindowsServicesStatusGetByPowershell();
--- SaltStack_API_Helper/Salt_API_Function.cs
+++ SaltStack_API_Helper/Salt_API_Function.cs
@@ -543,11 +543,11 @@ namespace SaltAPI
         /// <param name="svnPassword"> SVN 密码</param>
         /// <param name="minionName">Minion 机器名</param>
         /// <param name="version">版本</param>
-        /// <param name="source">源</param>
+        /// <param name="remote">源</param>
         /// <param name="st">使用自带模块还是私有模块</param>
         /// <param name="fun">使用私有模块的时候需要提供模块名称</param>
         /// <returns></returns>
-        public static string SVNOperation(string filePath, SVNOperation so, List<string> minionName, string svnUsername = "", string svnPassword = "", string remote = "", int? version = null, string source = "", SVNType st = SVNType.salt, string fun = "")
+        public static string SVNOperation(string filePath, SVNOperation so, List<string> minionName, string svnUsername = "", string svnPassword = "", string remote = "", int? version = null, SVNType st = SVNType.salt, string fun = "")
         {
             RunCmdType rct = new RunCmdType();
             rct.client = "local";
@@ -743,18 +743,19 @@ namespace SaltAPI
                                 List<string> bindTemp = new List<string>();
                                 var bindOneRow = i.SelectSingleNode("site/bindings").ChildNodes[j];
                                 var bindInfo = bindOneRow.Attributes["bindingInformation"].Value.Split(':');
-                                if (bindInfo.Length==2)
+                                if (bindInfo.Length == 2)
                                 {
                                     bindTemp.Add(bindInfo[1]);// IP 
                                     bindTemp.Add(bindInfo[0]);// port
                                     bindTemp.Add("");// domain
                                 }
-                                else if (bindInfo.Length==3){
+                                else if (bindInfo.Length == 3)
+                                {
                                     bindTemp.Add(bindInfo[0]);// IP 
                                     bindTemp.Add(bindInfo[1]);// port
                                     bindTemp.Add(bindInfo[2]);// domain
                                 }
-                                
+
 
                                 bindDict.Add(
                                     new Dictionary<string, List<string>>()
@@ -898,12 +899,12 @@ namespace SaltAPI
         /// <param name="ip">IP</param>
         /// <param name="port">端口</param>
         /// <returns></returns>
-        public static bool SiteBindEdit(string minionName, IISBindOperation ibo, string siteName,string domain,string ip,string port)
+        public static bool SiteBindEdit(string minionName, IISBindOperation ibo, string siteName, string domain, string ip, string port)
         {
-            if (!string.IsNullOrWhiteSpace(siteName) && !string.IsNullOrWhiteSpace(domain)&& !string.IsNullOrWhiteSpace(ip) && !string.IsNullOrWhiteSpace(port))
+            if (!string.IsNullOrWhiteSpace(siteName) && !string.IsNullOrWhiteSpace(domain) && !string.IsNullOrWhiteSpace(ip) && !string.IsNullOrWhiteSpace(port))
             {
                 string fun = "xjoker_win_iis.create_binding";
-                if (ibo==IISBindOperation.Delete)
+                if (ibo == IISBindOperation.Delete)
                 {
                     fun = "xjoker_win_iis.remove_binding";
                 }
@@ -913,10 +914,10 @@ namespace SaltAPI
                 rct.expr_form = "glob";
                 rct.tgt = minionName;
                 rct.fun = fun;
-                rct.arg = new List<string> { siteName, domain,ip,port };
+                rct.arg = new List<string> { siteName, domain, ip, port };
 
                 var r = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
-                if (r!=null && r.Keys.Contains(minionName))
+                if (r != null && r.Keys.Contains(minionName))
                 {
                     if (Convert.ToBoolean(r[minionName]))
                     {
@@ -928,5 +929,262 @@ namespace SaltAPI
             return false;
         }
 
+        /// <summary>
+        /// IIS 站点创建
+        /// </summary>
+        /// <param name="minions">选定的minion</param>
+        /// <param name="apppoolName">程序池名称</param>
+        /// <param name="apppoolVersion">程序池版本</param>
+        /// <param name="apppoolEnable32bit">是否启用32位模式</param>
+        /// <param name="apppoolModel">程序池模式</param>
+        /// <param name="siteName">站点名称</param>
+        /// <param name="siteProtocol">站点协议</param>
+        /// <param name="siteDomain">站点域名</param>
+        /// <param name="sitePath">站点路径</param>
+        /// <param name="siteLogPath">站点日志路径</param>
+        /// <param name="siteIP">站点IP</param>
+        /// <param name="sitePort">站点端口</param>
+        /// <param name="siteRunasUsername">站点运行用户</param>
+        /// <param name="siteRunasPassword">站点运行用户密码</param>
+        /// <param name="svnUrl">用于克隆站点文件的SVN地址</param>
+        /// <param name="svnUsername">用于克隆站点文件的SVN账户名</param>
+        /// <param name="svnPassword">用于克隆站点文件的SVN密码</param>
+        /// <param name="siteSourceGoodsync">Goodsync站点同步源地址</param>
+        /// <param name="siteSourceGoodsyncExclude">Goodsync站点同步排除列表</param>
+        /// <param name="siteSourceGoodsyncInclude">Goodsync站点同步包含列表</param>
+        /// <returns></returns>
+        public static Dictionary<string, bool> CreateIISSite(
+            List<string> minions,
+            string apppoolName="",
+            string apppoolVersion = "",
+            bool apppoolEnable32bit=false,
+            string apppoolModel = "",
+            string siteName = "",
+            string siteProtocol = "",
+            string siteDomain = "",
+            string sitePath = "",
+            string siteLogPath = "",
+            string siteIP = "",
+            string sitePort = "",
+            string siteRunasUsername = "",
+            string siteRunasPassword = "",
+            string svnUrl = "",
+            string svnUsername = "",
+            string svnPassword = "",
+            string siteSourceGoodsync = "",
+            string siteSourceGoodsyncExclude = "",
+            string siteSourceGoodsyncInclude = "")
+        {
+
+            if (minions.Count > 0)
+            {
+                // 初始化返回信息
+                Dictionary<string, bool> r = new Dictionary<string, bool>();
+                foreach (var item in minions)
+                {
+                    r.Add(item, false);
+                }
+
+                RunCmdType rct = new RunCmdType();
+                string tgt = string.Join(",", minions);// minion目标
+                string expr_from = "list"; // 默认的expr_from
+
+                // 如果站点IP为空，则设定为"*"
+                if (string.IsNullOrWhiteSpace(siteIP))
+                {
+                    siteIP = "*";
+                }
+
+                // 如果站点端口为空，则设定为"80"
+                if (string.IsNullOrWhiteSpace(sitePort))
+                {
+                    sitePort = "80";
+                }
+
+                // 如果站点对应的程序池名称未提供为空，则设定为同站点名
+                if (string.IsNullOrWhiteSpace(apppoolName))
+                {
+                    apppoolName = siteName;
+                }
+
+
+                // 从SVN拉取数据
+                if (!string.IsNullOrWhiteSpace(svnUrl))
+                {
+                    var svnPath = sitePath.Substring(0, sitePath.LastIndexOf("\\"));
+
+                    var bb = JsonConvert.DeserializeObject<Dictionary<string, string>>(SVNOperation(svnPath, SaltAPI.SVNOperation.checkout, minions, svnUsername, svnPassword, svnUrl));
+                    foreach (var i in minions)
+                    {
+                        if (bb.Keys.Contains(i))
+                        {
+                            // 如果SVN返回的结果含有error 则判断为更新失败
+                            // 不太准确
+                            if (bb[i].Contains("ERROR"))
+                            {
+                                r[i] = false;
+                            }
+                        }
+                    }
+                }
+
+                // 创建程序池
+                
+                rct.fun = "xjoker_win_iis.create_apppool";
+                rct.client = "local";
+                rct.expr_form = expr_from;
+                rct.tgt = minions;
+                rct.arg = new List<string> { apppoolName };
+                var b = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
+                foreach (var i in minions)
+                {
+                    if (b.Keys.Contains(i))
+                    {
+                        // 如果SVN返回的结果含有error 则判断为更新失败
+                        // 不太准确
+                        if (b[i].Contains("ERROR"))
+                        {
+                            r[i] = false;
+                        }
+                    }
+                }
+
+                // 设定程序池
+                rct.fun = "xjoker_win_iis.apppool_setting";
+                rct.client = "local";
+                rct.expr_form = expr_from;
+                rct.tgt = minions;
+                rct.arg = new List<string> { "name=" + apppoolName, "runtime_version=" + apppoolVersion, "pipeline_mode=" + apppoolModel, "bit_setting=" + apppoolEnable32bit.ToString() };
+                var bbb = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
+                foreach (var i in minions)
+                {
+                    if (bbb.Keys.Contains(i))
+                    {
+                        // 如果SVN返回的结果含有error 则判断为更新失败
+                        // 不太准确
+                        if (bbb[i].Contains("ERROR"))
+                        {
+                            r[i] = false;
+                        }
+                    }
+                }
+
+                // 创建站点
+                rct = new RunCmdType();
+                rct.fun = "xjoker_win_iis.create_site";
+                rct.client = "local";
+                rct.expr_form = expr_from;
+                rct.tgt = minions;
+                rct.arg = new List<string> { "name=" + siteName,
+                                    "protocol=" + siteProtocol,
+                                    "sourcepath=" + sitePath,
+                                    "port=" + sitePort,
+                                    "apppool=" + apppoolName,
+                                    "hostheader=" + siteDomain,
+                                    "ipaddress="+siteIP };
+                var c = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
+                foreach (var i in minions)
+                {
+                    if (c.Keys.Contains(i))
+                    {
+                        // 如果SVN返回的结果含有error 则判断为更新失败
+                        // 不太准确
+                        if (c[i].Contains("ERROR"))
+                        {
+                            r[i] = false;
+                        }
+                    }
+                }
+
+
+                //设定站点Runas
+                if (!string.IsNullOrWhiteSpace(siteRunasUsername)&&!string.IsNullOrWhiteSpace(siteRunasPassword))
+                {
+                    rct = new RunCmdType();
+                    rct.fun = "xjoker_win_iis.site_run_as";
+                    rct.client = "local";
+                    rct.expr_form = expr_from;
+                    rct.tgt = minions;
+                    rct.arg = new List<string> { siteName,siteRunasUsername,siteRunasPassword};
+                    var d = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
+                    foreach (var i in minions)
+                    {
+                        if (d.Keys.Contains(i))
+                        {
+                            // 如果SVN返回的结果含有error 则判断为更新失败
+                            // 不太准确
+                            if (d[i].Contains("ERROR"))
+                            {
+                                r[i] = false;
+                            }
+                        }
+                    }
+                }
+
+
+                //设置站点日志
+                if (!string.IsNullOrWhiteSpace(siteLogPath))
+                {
+                    rct = new RunCmdType();
+                    rct.fun = "xjoker_win_iis.site_log_path";
+                    rct.client = "local";
+                    rct.expr_form = expr_from;
+                    rct.tgt = minions;
+                    rct.arg = new List<string> { siteName, siteLogPath };
+                    var e = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
+                    foreach (var i in minions)
+                    {
+                        if (e.Keys.Contains(i))
+                        {
+                            // 如果SVN返回的结果含有error 则判断为更新失败
+                            // 不太准确
+                            if (e[i].Contains("ERROR"))
+                            {
+                                r[i] = false;
+                            }
+                        }
+                    }
+                }
+
+                // 设定goodsync
+                if (!string.IsNullOrWhiteSpace(siteSourceGoodsync))
+                {
+                    rct = new RunCmdType();
+                    rct.fun = "xjoker_goodsync.jobnew";
+                    rct.client = "local";
+                    rct.expr_form = expr_from;
+                    rct.tgt = minions;
+                    rct.arg = new List<string> { "jobname="+siteName,
+                                            "f1="+siteSourceGoodsync,
+                                            "f2="+sitePath,
+                                            "ReadOnlySource=0",
+                                            "exclude="+siteSourceGoodsyncExclude,
+                                            "include="+siteSourceGoodsyncInclude,
+                                            "OnFileChangeAction=1",
+                                            "OnTimerAction=1",
+                                            "DetectMovesAndRenames=0",
+                                            "Option=1" };
+                    var f = JsonConvert.DeserializeObject<Dictionary<string, string>>(CmdRunString(RunCmdTypeToString(rct)));
+                    foreach (var i in minions)
+                    {
+                        if (f.Keys.Contains(i))
+                        {
+                            // 如果SVN返回的结果含有error 则判断为更新失败
+                            // 不太准确
+                            if (f[i].Contains("ERROR"))
+                            {
+                                r[i] = false;
+                            }
+                        }
+                    }
+                }
+                return r;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
